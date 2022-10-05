e54f53231004a80da8d5e95ed427807ccf7f65cc
AppleGam1ng-ax <84991912+AppleGam1ng-ax@users.noreply.github.com>
2022-10-05 13:28:48 +0200

    Update main loader.lua

--- main loader.lua
+++ main loader.lua
@@ -1,53 +1,37 @@
-repeat wait() until game:IsLoaded()
-
-if game.PlaceId == 6808416928 then
+if game.PlaceId == 6808416928 then --Aimblox
     loadstring(game:HttpGet("https://raw.githubusercontent.com/AppleGam1ng-ax/script/main/Aimb.lua"))()
-else
-    if game.PlaceId == 662417684 then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/AppleGam1ng-ax/script/main/BlocksWhichareluclky.lua"))()
-    else
-        if game.PlaceId == 9414511685 then
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/AppleGam1ng-ax/script/main/Drivecarsdown.lua"))()
-        else
-            if game.PlaceId == 3956818381 then
-                loadstring(game:HttpGet("https://raw.githubusercontent.com/AppleGam1ng-ax/script/main/NinjasArelegnds2.lua"))()
-            else
-                if game.PlaceId == 3956818381 then
-                    loadstring(game:HttpGet("https://raw.githubusercontent.com/AppleGam1ng-ax/script/main/PewPewSim.lua"))()
-                else
-                    if game.PlaceId == 2512643572 then
-                        loadstring(game:HttpGet("https://raw.githubusercontent.com/AppleGam1ng-ax/script/main/bgs.lua"))()
-                    else
-                        if game.PlaceId == 2629642516 then
-                            loadstring(game:HttpGet("https://raw.githubusercontent.com/AppleGam1ng-ax/script/main/btcminersim.lua"))()
-                        else
-                            if game.PlaceId == 10320240572 then
-                                loadstring(game:HttpGet("https://raw.githubusercontent.com/AppleGam1ng-ax/script/main/ezraceclick.lua"))()
-                            else
-                                if game.PlaceId == 7952502098 then
-                                    loadstring(game:HttpGet("https://raw.githubusercontent.com/AppleGam1ng-ax/script/main/glassbridge!.lua"))()
-                                else
-                                    if game.PlaceId == 3956818381 then
-                                        loadstring(game:HttpGet("https://raw.githubusercontent.com/AppleGam1ng-ax/script/main/ninjasarelegends1.lua"))()
-                                    else
-                                        if game.PlaceId == 155615604 then
-                                            loadstring(game:HttpGet("https://raw.githubusercontent.com/AppleGam1ng-ax/script/main/originJB.lua"))()
-                                        else
-                                            if game.PlaceId == 292439477 then
-                                                loadstring(game:HttpGet("https://raw.githubusercontent.com/AppleGam1ng-ax/script/main/wallha.lua"))()
-                                            else
-                                                if game.PlaceId == 2693023319 then
-                                                    loadstring(game:HttpGet("https://raw.githubusercontent.com/AppleGam1ng-ax/script/main/highmounttaincliminglol.lua"))()
-                                                end
-                                            end
-                                        end
-                                    end
-                                end
-                            end
-                        end
-                    end
-                end
-            end
-        end
-    end
+
+    elseif game.PlaceId == 2512643572 then  --Bubble Gum Simulator
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AppleGam1ng-ax/script/main/bgs.lua"))()
+
+    elseif game.PlaceId == 10320240572 then --Easy Race Clicker
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AppleGam1ng-ax/script/main/ezraceclick.lua"))()
+
+    elseif game.PlaceId == 7952502098 then --Impossible glass bridge obby
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AppleGam1ng-ax/script/main/glassbridge!.lua"))()
+
+    elseif game.PlaceId == 662417684 then --Lucky Blocks
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AppleGam1ng-ax/script/main/BlocksWhichareluclky.lua"))()
+
+    elseif game.PlaceId == 5977280685 then --Ninja Legends 2
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AppleGam1ng-ax/script/main/NinjasArelegnds2.lua"))()
+
+    elseif game.PlaceId == 3956818381 then --Ninja Legends 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AppleGam1ng-ax/script/main/ninjasarelegends1.lua"))()
+
+    elseif game.PlaceId == 155615604 then --Prison life
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AppleGam1ng-ax/script/main/originJB.lua"))()
+
+    else if game.PlaceId == 9414511685 then --Drive Down Cars A Hill
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AppleGam1ng-ax/script/main/Drivecarsdown.lua"))()
+
+    elseif game.PlaceId == 2629642516 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AppleGam1ng-ax/script/main/btcminersim.lua"))()
+
+    elseif game.PlaceId == 2788229376 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AppleGam1ng-ax/script/main/PewPewSim.lua"))()
+
+    elseif game.PlaceId  == 2693023319 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/AppleGam1ng-ax/script/main/highmounttaincliminglol.lua"))()
+end
 end
